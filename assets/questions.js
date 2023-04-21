60d5b7aee016acec8d032a0ed4878ac2eee26fdf
Renan <rs.miranda93@gmail.com>
2023-04-20 21:19:17 -0400

    Updated question 1_2 and README file with credit to the music

--- assets/questions.js
+++ assets/questions.js
@@ -1,26 +1,28 @@
 // creating an array and passing the number, questions, options, and answers
+// creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
-    numb: 1,
-    question: "Commonly used data types DO Not Include:",
-    answer: "3. alerts",
-    options: [
-      "1. Strings",
-      "2. Booleans",
-      "3. alerts",
-      "4. numbers"
-    ]
-  },
-    {
-    numb: 2,
-    question: "The condition in an if/else statement is enclosed with ________.",
-    answer: "3. parenthesis",
-    options: [
-      "1. quotes",
-      "2. curly brackets",
-      "3. parenthesis",
-      "4. square brackets"
-    ]
-  }
-  
-  ];
+  {
+  numb: 1,
+  question: "The city that I was born in the US is also a city in Canada. Do you know which city I am from?",
+  answer: "3. Burlington",
+  options: [
+    "1. Vancouver",
+    "2. Victoria",
+    "3. Burlington",
+    "4. Richmond"
+  ]
+}
+//   {
+//   numb: 2,
+//   question: "The condition in an if/else statement is enclosed with ________.",
+//   answer: "3. parenthesis",
+//   options: [
+//     "1. quotes",
+//     "2. curly brackets",
+//     "3. parenthesis",
+//     "4. square brackets"
+//   ]
+// }
+
+];
+
